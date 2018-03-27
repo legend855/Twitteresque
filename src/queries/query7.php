fa5821ec16f8b632df27a7f3a84b2f2b687f683a
Patrick Kyoyetera <pertriquek171@gmail.com>
2018-03-27 13:49:20 -0400

    Query 7

--- src/queries/query7.php
+++ src/queries/query7.php
@@ -11,37 +11,39 @@
 
 <body>
 <?php
-    // after log in, we retain user's details
+
+    // after log in, we retain user's details in variables
     require('../utils/login.php');
 
-    // get tweet contents form input
-    $msg = 1;
-    
-    $query = "INSERT INTO twitts (body)
-                VALUES ('$msg')";
+    // find a user's id and return it
+    function getUserID($con, $username) {
+        $qu = "SELECT uid FROM user WHERE username='$username'";
+        $res = mysqli_query($con, $qu);
+        $id = mysqli_fetch_row($res);
+        return $id[0]; 
+    }
+
+    // get tweet contents from input
+    $tweet = $_POST['tweet_body'];
+    $date = date("Y-m-d h:i:s");
+    $user_id = getUserID($con, $username);
+
+    $query = "INSERT INTO twitts (uid, body, post_time)
+                VALUES ('$user_id', '$msg', '$date')";
     
     $result = mysqli_query($con, $query)
                 or die('Query failed: '. mysqli_error($con));
 
-
-    // print number of results in the query?
-    echo mysqli_num_rows($result);
-    echo "<br>";
-    //echo mysqli_field_count($result);
-    //echo "<br>";
-
-    $count = 0;
-    // print results from query
-    while($row = mysqli_fetch_row($result)) {
-
+    if($result == true) {
+        echo "Tweet posted Successfully!";
+    } else {
+        echo "Failed to post tweet";
     }
+
     mysqli_free_result($result);
     mysqli_close($con);
     
-    
-    require('logout.php');
-
-
+    require('../utils/logout.php');
 ?>
 
     
